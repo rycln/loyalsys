dd9629dbc870f49a7b82a6f8795ba1b22bb46bea
Khalikov Ruslan <rycln1@gmail.com>
2025-04-17 11:38:03 +0300

    Updated storage tests

--- internal/storage/orderstorage_test.go
+++ internal/storage/orderstorage_test.go
@@ -26,6 +26,8 @@ func TestOrderStorage_AddOrder(t *testing.T) {
 	}
 	defer db.Close()
 
+	strg := NewOrderStorage(db)
+
 	testOrder := &models.Order{
 		Number: testOrderNum,
 		UserID: testUserID,
@@ -34,17 +36,17 @@ func TestOrderStorage_AddOrder(t *testing.T) {
 	t.Run("valid test", func(t *testing.T) {
 		mock.ExpectExec("INSERT INTO orders").WithArgs(testOrder.Number, testOrder.UserID).WillReturnResult(sqlmock.NewResult(1, 1))
 
-		strg := NewOrderStorage(db)
 		err := strg.AddOrder(context.Background(), testOrder)
 		assert.NoError(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("some error", func(t *testing.T) {
 		mock.ExpectExec("INSERT INTO orders").WithArgs(testOrder.Number, testOrder.UserID).WillReturnError(errTest)
 
-		strg := NewOrderStorage(db)
 		err := strg.AddOrder(context.Background(), testOrder)
 		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 }
 
@@ -54,6 +56,8 @@ func TestOrderStorage_GetOrderByNum(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
+
+	strg := NewOrderStorage(db)
 
 	testOrder := &models.OrderDB{
 		ID:        testOrderID,
@@ -68,25 +72,172 @@ func TestOrderStorage_GetOrderByNum(t *testing.T) {
 		rows := mock.NewRows([]string{"id", "number", "user_id", "status", "accrual", "created_at"}).AddRow(testOrder.ID, testOrder.Number, testOrder.UserID, testOrder.Status, testOrder.Accrual, testOrder.CreatedAt)
 		mock.ExpectQuery("SELECT id, number, user_id, status, accrual, created_at FROM orders").WithArgs(testOrder.Number).WillReturnRows(rows)
 
-		strg := NewOrderStorage(db)
 		orderDB, err := strg.GetOrderByNum(context.Background(), testOrder.Number)
 		assert.NoError(t, err)
 		assert.Equal(t, testOrder, orderDB)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("no order error", func(t *testing.T) {
 		mock.ExpectQuery("SELECT id, number, user_id, status, accrual, created_at FROM orders").WithArgs(testOrder.Number).WillReturnError(sql.ErrNoRows)
 
-		strg := NewOrderStorage(db)
 		_, err := strg.GetOrderByNum(context.Background(), testOrder.Number)
 		assert.ErrorIs(t, err, ErrNoOrder)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("some error", func(t *testing.T) {
 		mock.ExpectQuery("SELECT id, number, user_id, status, accrual, created_at FROM orders").WithArgs(testOrder.Number).WillReturnError(errTest)
 
-		strg := NewOrderStorage(db)
 		_, err := strg.GetOrderByNum(context.Background(), testOrder.Number)
 		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
+
+func TestOrderStorage_GetOrdersByUserID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	strg := NewOrderStorage(db)
+
+	testOrder := &models.OrderDB{
+		Number:    testOrderNum,
+		Status:    "some status",
+		Accrual:   0,
+		CreatedAt: testCreatedAt.String(),
+	}
+
+	t.Run("valid test", func(t *testing.T) {
+		rows := mock.NewRows([]string{"number", "status", "accrual", "created_at"}).
+			AddRow(testOrder.Number, testOrder.Status, testOrder.Accrual, testOrder.CreatedAt)
+		mock.ExpectQuery("SELECT number, status, accrual, created_at FROM orders").WithArgs(testUserID).WillReturnRows(rows)
+
+		orderDB, err := strg.GetOrdersByUserID(context.Background(), testUserID)
+		assert.NoError(t, err)
+		assert.Equal(t, testOrder, orderDB[0])
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("some error", func(t *testing.T) {
+		mock.ExpectQuery("SELECT number, status, accrual, created_at FROM orders").WithArgs(testUserID).WillReturnError(errTest)
+
+		_, err := strg.GetOrdersByUserID(context.Background(), testUserID)
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("empty response", func(t *testing.T) {
+		rows := mock.NewRows([]string{"number", "status", "accrual", "created_at"})
+		mock.ExpectQuery("SELECT number, status, accrual, created_at FROM orders").WillReturnRows(rows)
+
+		_, err := strg.GetOrdersByUserID(context.Background(), testUserID)
+		assert.ErrorIs(t, err, ErrNoOrder)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
+
+func TestOrderStorage_GetInconclusiveOrderNums(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	strg := NewOrderStorage(db)
+
+	orderNums := []string{
+		"123",
+		"456",
+	}
+
+	t.Run("valid test", func(t *testing.T) {
+		rows := mock.NewRows([]string{"number"}).AddRow(orderNums[0]).AddRow(orderNums[1])
+		mock.ExpectQuery("SELECT number FROM orders").WillReturnRows(rows)
+
+		nums, err := strg.GetInconclusiveOrderNums(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, orderNums, nums)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("some error", func(t *testing.T) {
+		mock.ExpectQuery("SELECT number FROM orders").WillReturnError(errTest)
+
+		_, err := strg.GetInconclusiveOrderNums(context.Background())
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+}
+
+func TestOrderStorage_UpdateOrdersBatch(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	strg := NewOrderStorage(db)
+
+	testOrders := []*models.OrderDB{
+		{
+			Number:    "123",
+			Status:    "some status",
+			Accrual:   10,
+			CreatedAt: time.Now().String(),
+		},
+		{
+			Number:    "456",
+			Status:    "some status",
+			Accrual:   10,
+			CreatedAt: time.Now().String(),
+		},
+	}
+
+	t.Run("valid test", func(t *testing.T) {
+		mock.ExpectBegin()
+		mockStmt := mock.ExpectPrepare("UPDATE orders")
+		for _, testOrder := range testOrders {
+			mockStmt.ExpectExec().WithArgs(testOrder.Status, testOrder.Accrual, testOrder.Number).WillReturnResult(sqlmock.NewResult(1, 1))
+		}
+		mock.ExpectCommit()
+
+		err := strg.UpdateOrdersBatch(context.Background(), testOrders)
+		assert.NoError(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("begin error", func(t *testing.T) {
+		mock.ExpectBegin().WillReturnError(errTest)
+
+		err := strg.UpdateOrdersBatch(context.Background(), testOrders)
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("prepare error", func(t *testing.T) {
+		mock.ExpectBegin()
+		mock.ExpectPrepare("UPDATE orders").WillReturnError(errTest)
+		mock.ExpectRollback()
+
+		err := strg.UpdateOrdersBatch(context.Background(), testOrders)
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
+	})
+
+	t.Run("exec error", func(t *testing.T) {
+		mock.ExpectBegin()
+		mockStmt := mock.ExpectPrepare("UPDATE orders")
+
+		mockStmt.ExpectExec().WithArgs(testOrders[0].Status, testOrders[0].Accrual, testOrders[0].Number).WillReturnError(errTest)
+
+		mock.ExpectRollback()
+
+		err := strg.UpdateOrdersBatch(context.Background(), testOrders)
+		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 }
--- internal/storage/userstorage_test.go
+++ internal/storage/userstorage_test.go
@@ -20,6 +20,8 @@ func TestUserStorage_AddUser(t *testing.T) {
 	}
 	defer db.Close()
 
+	strg := NewUserStorage(db)
+
 	testUser := &models.UserDB{
 		Login:        "test",
 		PasswordHash: "hashed_password",
@@ -29,10 +31,10 @@ func TestUserStorage_AddUser(t *testing.T) {
 		rows := mock.NewRows([]string{"id"}).AddRow(testUserID)
 		mock.ExpectQuery("INSERT INTO users").WithArgs(testUser.Login, testUser.PasswordHash).WillReturnRows(rows)
 
-		strg := NewUserStorage(db)
 		uid, err := strg.AddUser(context.Background(), testUser)
 		assert.NoError(t, err)
 		assert.Equal(t, testUserID, uid)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("conflict error", func(t *testing.T) {
@@ -43,18 +45,18 @@ func TestUserStorage_AddUser(t *testing.T) {
 		rows := mock.NewRows([]string{"id"}).AddRow(testUserID).RowError(0, pgErr)
 		mock.ExpectQuery("INSERT INTO users").WithArgs(testUser.Login, testUser.PasswordHash).WillReturnRows(rows)
 
-		strg := NewUserStorage(db)
 		_, err = strg.AddUser(context.Background(), testUser)
 		assert.ErrorIs(t, err, ErrLoginConflict)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("some error", func(t *testing.T) {
 		rows := mock.NewRows([]string{"id"}).AddRow(testUserID).RowError(0, errTest)
 		mock.ExpectQuery("INSERT INTO users").WithArgs(testUser.Login, testUser.PasswordHash).WillReturnRows(rows)
 
-		strg := NewUserStorage(db)
 		_, err = strg.AddUser(context.Background(), testUser)
 		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 }
 
@@ -64,6 +66,8 @@ func TestUserStorage_GetUserByLogin(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer db.Close()
+
+	strg := NewUserStorage(db)
 
 	testUser := &models.UserDB{
 		ID:           testUserID,
@@ -75,25 +79,25 @@ func TestUserStorage_GetUserByLogin(t *testing.T) {
 		rows := mock.NewRows([]string{"id", "login", "password_hash"}).AddRow(testUser.ID, testUser.Login, testUser.PasswordHash)
 		mock.ExpectQuery("SELECT id, login, password_hash FROM users").WithArgs(testUser.Login).WillReturnRows(rows)
 
-		strg := NewUserStorage(db)
 		userDB, err := strg.GetUserByLogin(context.Background(), testUser.Login)
 		assert.NoError(t, err)
 		assert.Equal(t, testUser, userDB)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("no user error", func(t *testing.T) {
 		mock.ExpectQuery("SELECT id, login, password_hash FROM users").WithArgs(testUser.Login).WillReturnError(sql.ErrNoRows)
 
-		strg := NewUserStorage(db)
 		_, err := strg.GetUserByLogin(context.Background(), testUser.Login)
 		assert.ErrorIs(t, err, ErrNoUser)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 
 	t.Run("some error", func(t *testing.T) {
 		mock.ExpectQuery("SELECT id, login, password_hash FROM users").WithArgs(testUser.Login).WillReturnError(errTest)
 
-		strg := NewUserStorage(db)
 		_, err := strg.GetUserByLogin(context.Background(), testUser.Login)
 		assert.Error(t, err)
+		assert.NoError(t, mock.ExpectationsWereMet())
 	})
 }
